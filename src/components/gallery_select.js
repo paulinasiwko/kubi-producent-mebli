da3234f20504ab907ef8ae8539693255e4c63dae
Paulina Siwko <pau.siwko@gmail.com>
2023-02-20 11:16:33 +0100

    gallery styling

--- src/components/gallery_select.js
+++ src/components/gallery_select.js
@@ -6,47 +6,47 @@ export default function GallerySelect({ firstCard, secondCard, thirdCard, fourth
   return(
     <>
       <Row className='galleryRow'>
-        <Col>
-          <Card style={{width: '18rem', height: '18rem'}}>
+        <Col className='col'>
+          <Card className='shadow' style={{width: '18rem', height: '19rem'}}>
             <Card.Img src={require('../img/szafy.jpg')} />
             <Card.Body>
               <Card.Text style={{textAlign: 'center'}}>
                 <p style={{fontWeight: 'bold'}}>{firstCard}</p>
               </Card.Text>
-              <Button variant='light'>Zobacz zdjęcia</Button>
+              <Button className='cardButton' variant='light'>Zobacz zdjęcia</Button>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card style={{width: '18rem', height: '18rem'}}>
+          <Card className='shadow' style={{width: '18rem', height: '19rem'}}>
             <Card.Img src={require('../img/szafy.jpg')} />
             <Card.Body>
               <Card.Text style={{textAlign: 'center'}}>
                 <p style={{fontWeight: 'bold'}}>{secondCard}</p>
               </Card.Text>
-              <Button variant='light'>Zobacz zdjęcia</Button>
+              <Button className='cardButton' variant='light'>Zobacz zdjęcia</Button>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card style={{width: '18rem', height: '18rem'}}>
+          <Card className='shadow' style={{width: '18rem', height: '19rem'}}>
             <Card.Img src={require('../img/lazienka.jpg')} />
             <Card.Body>
               <Card.Text style={{textAlign: 'center'}}>
                 <p style={{fontWeight: 'bold'}}>{thirdCard}</p>
               </Card.Text>
-              <Button variant='light'>Zobacz zdjęcia</Button>
+              <Button className='cardButton' variant='light'>Zobacz zdjęcia</Button>
             </Card.Body>
           </Card>
         </Col>
         <Col>
-          <Card style={{width: '18rem', height: '18rem'}}>
+          <Card className='shadow' style={{width: '18rem', height: '19rem'}}>
             <Card.Img src={require('../img/lazienka.jpg')} />
             <Card.Body>
               <Card.Text style={{textAlign: 'center'}}>
                 <p style={{fontWeight: 'bold'}}>{fourthCard}</p>
               </Card.Text>
-              <Button variant='light'>Zobacz zdjęcia</Button>
+              <Button className='cardButton' variant='light'>Zobacz zdjęcia</Button>
             </Card.Body>
           </Card>
         </Col>
--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -9,9 +9,10 @@ export default function GalleryPage() {
     <>
       <AdditionalNavigationMenu menuText={'Galeria'} />
       <h1 className='mt-3' style={{color: 'white', textAlign: 'center'}}>Galeria</h1>
-      <p className='mt-5' style={{color: 'white', textAlign: 'center'}}>
+      <p className='mt-5 paragraph' style={{color: 'white', textAlign: 'center'}}>
         Kuchnia to miejsce, w którym spędzamy większość czasu – tu przygotowujemy posiłki, jemy, ale też bardzo często po prostu siedzimy przy kawie i rozmawiamy z rodziną. Można zatem śmiało powiedzieć, że jest to pomieszczenie, które powinno być najbardziej reprezentatywne i urządzone w taki sposób, aby każdy z domowników i gości czuł się tu dobrze. Meble kuchenne to pierwszy i najważniejszy z aspektów przy urządzaniu tego wnętrza. Meble kuchenne powinny być wykonane z trwałego materiału oraz wyróżniać się nieskazitelnym wyglądem.
       </p>
+      <div className='borderBottom'></div>
       <div className='gallerySelect'>
         <GallerySelect
           firstCard={'Kuchnie'}
