103218cc93c98311e426157359584be98729c5cb
Paulina Siwko <pau.siwko@gmail.com>
2023-03-07 13:34:07 +0100

    responsive footer with react bootstrap

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,75 +2,36 @@ import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookSquare} from "@fortawesome/free-brands-svg-icons";
 import { FaPhoneAlt, FaMapMarkerAlt, FaEnvelope } from "react-icons/fa";
-import {Container, Nav, Navbar, NavLink } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import {Container, Row, Col } from 'react-bootstrap';
 
-/*
-export default function Footer() {
-  return (
-    <Navbar>
-    <Container
-      fluid
-      style={{backgroundColor: 'black', color: 'white'}}>
-      <Nav style={{margin: 'auto', width: '800px'}}>
-        <div style={{margin: '18px 0'}}>
-          <FaMapMarkerAlt size='20' />
-        </div>
-        <p style={{padding: '0 20px 0 10px',
-          margin: '20px 20px 20px 0',
-          borderRight: '1px solid white'}}>ul. Jasna 6, 82-335 Jegłownik</p>
-        <div style={{margin: '18px 0'}}>
-          <FaPhoneAlt size='20' />
-        </div>
-        <p style={{padding: '0 20px 0 10px',
-          margin: '20px 20px 20px 0',
-          borderRight: '1px solid white'}}>(+48) 502-472-763</p>
-        <div style={{margin: '18px 0'}}>
-          <FaEnvelope size='20' />
-        </div>
-        <p style={{padding: '0 20px 0 10px',
-          margin: '20px 20px 20px 0',
-          borderRight: '1px solid white'}}> kubimeble@icloud.com</p>
-        <div style={{margin: '18px 0'}}>
-          <a href='https://www.facebook.com/profile.php?id=100063485287584'>
-            <FontAwesomeIcon icon={faFacebookSquare} size='2x' />
-          </a>
-        </div>
-      </Nav>
-    </Container>
-    </Navbar>
-  );
-}
-*/
 
 export default function Footer() {
   return (
       <Container
         fluid
-        style={{backgroundColor: 'black', color: 'white'}}>
-        <Nav style={{margin: 'auto', textAlign: 'center'}}
-             className='flex-column'>
-          <p style={{padding: '0 10px',
-            margin: '20px'}}><FaMapMarkerAlt size='20' /> ul. Jasna 6, 82-335 Jegłownik</p>
-          <div style={{borderBottom: '1px solid white',
-            width: '180px',
-            margin: 'auto'}} />
-          <p style={{padding: '0 10px',
-            margin: '20px'}}><FaPhoneAlt size='20' /> (+48) 502-472-763</p>
-          <div style={{borderBottom: '1px solid white',
-            width: '180px',
-            margin: 'auto'}} />
-          <p style={{padding: '0 10px',
-            margin: '20px'}}><FaEnvelope size='20' /> kubimeble@icloud.com</p>
-          <div style={{borderBottom: '1px solid white',
-            width: '180px',
-            margin: 'auto'}} />
-          <div style={{margin: '20px 0'}}>
+        style={{backgroundColor: 'black',
+          color: 'white',
+          textAlign: 'center',
+          padding: '20px 0'}}>
+        <Row style={{marginRight: '20px', marginLeft: '20px'}}>
+          <Col md='3' sm='6' xs='12' style={{borderLeft: '1px solid white', borderRight: '1px solid white'}}>
+            <FaMapMarkerAlt size='20' />
+            <p>ul. Jasna 6, 82-335 Jegłownik</p>
+          </Col>
+          <Col md='3' sm='6' xs='12' style={{borderLeft: '1px solid white', borderRight: '1px solid white'}}>
+            <FaPhoneAlt size='20' />
+            <p>(+48) 502-472-763</p>
+          </Col>
+          <Col md='3' sm='6' xs='12' style={{borderLeft: '1px solid white', borderRight: '1px solid white'}}>
+            <FaEnvelope size='20' />
+          <p>kubimeble@icloud.com</p>
+          </Col>
+          <Col md='3' sm='6' xs='12' style={{borderLeft: '1px solid white', borderRight: '1px solid white'}}>
             <a href='https://www.facebook.com/profile.php?id=100063485287584'>
-              <FontAwesomeIcon icon={faFacebookSquare} size='2x' />
+              <FontAwesomeIcon icon={faFacebookSquare} size='2x' style={{marginTop: '10px'}} />
             </a>
-          </div>
-        </Nav>
+          </Col>
+        </Row>
       </Container>
   );
 }
